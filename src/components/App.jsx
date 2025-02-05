b01bfe185fcfeda4ef0ddfe063a11c8a7bd39bdc
Alecu Marco <136191501+MarcoPolo236@users.noreply.github.com>
2025-02-05 11:03:52 +0200

    1st attempt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import React from "react";
+import { FeedbackWidget } from "./Feedback Widget";
+
 export const App = () => {
-  
+  return (
+    <div>
+      <FeedbackWidget
+      />
+    </div>
+  );
 };
---
+++ src/components/Feedback Widget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const FeedbackWidget = ({ state, good, neutral, bad }) => {
+    return (
+        state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+    );
+}
+
