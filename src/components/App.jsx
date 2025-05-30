16622ce48b5208ff5200066ca9123ea764224dde
Alecu Marco <mark_hero13@yahoo.com>
2025-05-30 20:17:30 +0300

    more updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-import { FeedbackWidget } from "./Feedback/Feedback Widget";
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './Feedback/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Wrapper } from './Section/SectionStyled';
+import { Notification } from './Notification/NitificationMessage';
 
 export class App extends Component {
   state = {
@@ -7,4 +11,53 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
-};
+
+  onHandleClick = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <Wrapper>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.onHandleClick}
+          />
+        </Section>
+
+        {total > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Wrapper>
+    );
+  }
+}
